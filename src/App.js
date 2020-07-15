9a079a7e5ce421588c5fa18695b0a59808130d01
Eugene Syrotiuk <eugenesyuk@gmail.com>
2020-07-15 17:56:36 +0200

    Added search by monsters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { Search } from './components/search/search.component';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -15,10 +17,18 @@ class App extends Component {
     this.setState({ monsters: users });
   }
 
+  handleSearchChange = (e) => {
+    return this.setState({ searchField: e.target.value });
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}/>
+        <Search placeholder='Search Monsters' changeHandler={this.handleSearchChange}/>
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
