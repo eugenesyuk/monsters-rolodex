fb6591d5a296b87775391bdb99a8f59304623eab
Eugene Syrotiuk <eugenesyuk@gmail.com>
2020-07-17 16:55:43 +0200

    Final styling

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <Search placeholder='Search Monsters' changeHandler={this.handleSearchChange}/>
         <CardList monsters={filteredMonsters}/>
       </div>
